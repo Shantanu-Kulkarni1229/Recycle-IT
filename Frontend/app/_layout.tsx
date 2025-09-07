06e12bb934516a1dca64176b5b53de6fb72c8b12
Shantanu-Kulkarni1229 <shantanukulkarni1229@gmail.com>
2025-09-07 15:57:51 +0530

    chore: remove frontend directory and all its contents
    This commit removes the entire Frontend directory including all React Native/Expo project files, assets, configurations, and authentication components. The removal is part of a project restructuring or cleanup effort.

--- Frontend/app/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Stack } from "expo-router";
-import "./global.css";
-
-const Layout: React.FC = () => {
-  return <Stack screenOptions={{ headerShown: false }} />;
-};
-
-export default Layout;
